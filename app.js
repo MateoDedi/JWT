c956b1d687313c695f043a82b929a227f5314d4f
MateoDedi <mateod@live.com>
2023-11-28 12:28:36 +0100

    .

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://mateo:12345@cluster0.fjybhh6.mongodb.net/node-auth';
+const dbURI = 'mongodb+srv://mateo:12345@cluster0.fjybhh6.mongodb.net/node-auth'; //home pc
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
@@ -25,21 +25,3 @@ app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
 // cookies
-app.get('/set-cookies', (req, res) => {
-
-  //res.setHeader('Set-Cookie', 'newUser=true');
-  res.cookie('newUser', false);
-  res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true});
-
-  res.send('you got the cookies');
-
-});
-
-app.get('/read-cookies', (req, res) => {
-
-  const cookies = req.cookies;
-  console.log(cookies.newUser);
-
-  res.json(cookies);
-
-});
